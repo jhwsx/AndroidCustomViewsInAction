41e535b1d4256479d2a834199e60d19afc4e9e43
wangzhichao <wangzhichao@adups.com>
2019-09-17 22:18:14 +0800

    refactor codes.

--- chaper06/src/main/java/com/example/chaper06/MainActivity.java
+++ chaper06/src/main/java/com/example/chaper06/MainActivity.java
@@ -3,34 +3,64 @@ package com.example.chaper06;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private List<PageModel> pageModels = new ArrayList<>();
 
+    {
+        pageModels.add(new PageModel(R.string.title_draw_text_line, R.layout.practice_text_line));
+        pageModels.add(new PageModel(R.string.title_draw_text_rect, R.layout.practice_text_rect));
+        pageModels.add(new PageModel(R.string.title_text_top_baseline, R.layout.practice_text_top_baseline));
+        pageModels.add(new PageModel(R.string.title_text_center_baseline, R.layout.practice_text_center_baseline));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.pager);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return PageFragment.newInstance(pageModels.get(position).layoutRes);
+            }
 
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return getString(pageModels.get(position).titleRes);
+            }
+
+            @Override
+            public int getCount() {
+                return pageModels.size();
+            }
+        });
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    public void text(View view) {
-        TextActivity.start(this);
+    private class PageModel {
+        @StringRes int titleRes;
+        @LayoutRes int layoutRes;
+
+        public PageModel(int titleRes, int layoutRes) {
+            this.titleRes = titleRes;
+            this.layoutRes = layoutRes;
+        }
     }
 
-    public void textarea(View view) {
-        TextAreaActivity.start(this);
-    }
-
-    public void top2baseline(View view) {
-        Top2BaseLineActivity.start(this);
-    }
-
-    public void center2baseline(View view) {
-        Center2BaseLineActivity.start(this);
-    }
-
-    public void paintbasic(View view) {
-        PaintBasicActivity.start(this);
-    }
 }
