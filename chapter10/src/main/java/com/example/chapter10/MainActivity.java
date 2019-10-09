65193b010040d4aa01dd7fa10824775474420346
wangzhichao <wangzhichao@adups.com>
2019-10-09 21:42:05 +0800

    chapter10 init.

---
+++ chapter10/src/main/java/com/example/chapter10/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.chapter10;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyViewPager viewPager;
+    private TabLayout tabLayout;
+    private List<PageModel> pageModels = new ArrayList<>();
+
+    {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.pager);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return PageFragment.newInstance(pageModels.get(position).layoutRes);
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return getString(pageModels.get(position).titleRes);
+            }
+
+            @Override
+            public int getCount() {
+                return pageModels.size();
+            }
+        });
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class PageModel {
+        @StringRes
+        int titleRes;
+        @LayoutRes
+        int layoutRes;
+
+        public PageModel(int titleRes, int layoutRes) {
+            this.titleRes = titleRes;
+            this.layoutRes = layoutRes;
+        }
+    }
+}
---
+++ chapter10/src/main/java/com/example/chapter10/MyViewPager.java
@@ -0,0 +1,35 @@
+package com.example.chapter10;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+/**
+ * @author wangzhichao
+ * @date 2019/09/19
+ */
+public class MyViewPager extends ViewPager {
+    public MyViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    private boolean noScroll;
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (noScroll) {
+            return false;
+        } else {
+            return super.onInterceptTouchEvent(ev);
+        }
+    }
+}
