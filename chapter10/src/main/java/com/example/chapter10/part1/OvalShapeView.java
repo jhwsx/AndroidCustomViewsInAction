9e83565ecff04468f864181b35f8e0c91f85182b
wangzhichao <wangzhichao@adups.com>
2019-10-11 21:59:13 +0800

    chapter10 OvalShape.

---
+++ chapter10/src/main/java/com/example/chapter10/part1/OvalShapeView.java
@@ -0,0 +1,36 @@
+package com.example.chapter10.part1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author wangzhichao
+ * @date 2019/10/11
+ */
+public class OvalShapeView extends View {
+    private ShapeDrawable drawable;
+    public OvalShapeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        drawable = new ShapeDrawable(new OvalShape());
+        drawable.setBounds(new Rect(50,50,200,100));
+        drawable.getPaint().setColor(Color.YELLOW);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawable.draw(canvas);
+    }
+}
+
+/**
+ * 总结：
+ * 1，OvalShape 是继承于 RectShape 的
+ */
