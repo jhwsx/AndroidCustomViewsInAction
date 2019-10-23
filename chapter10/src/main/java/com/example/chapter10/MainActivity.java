93021f88872638f634599c3fd8889353c9886032
wangzhichao <wangzhichao@adups.com>
2019-10-23 23:19:16 +0800

    chapter10 BitmapFactory.Options.inPreferredConfig.

--- chapter10/src/main/java/com/example/chapter10/MainActivity.java
+++ chapter10/src/main/java/com/example/chapter10/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         pageModels.add(new PageModel(R.string.title_sdcardbitmap_drawablebitmap, R.layout.practice_sdcardbitmap_drawablebitmap_view));
         pageModels.add(new PageModel(R.string.title_bitmapfactory_options_inscaled, R.layout.practice_bitmapfactory_options_inscaled));
         pageModels.add(new PageModel(R.string.title_bitmapfactoryoptionsindensityintargetdensityview, R.layout.practice_bitmapfactoryoptionsindensityintargetdensityview));
+        pageModels.add(new PageModel(R.string.title_bitmapfactoryoptionsinpreferredconfigview, R.layout.practice_bitmapfactoryoptionsinpreferredconfigview));
     }
 
     @Override
