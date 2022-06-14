946aa59866cb969b01c4734729022a089311c9e5
wangzhichao <willwaywang6@gmail.com>
2022-06-14 13:24:33 +0800

    chapter12

---
+++ chapter12/src/main/java/com/example/chapter12/CustomViewAttributeDetailViewGroup.java
@@ -0,0 +1,12 @@
+package com.example.chapter12;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class CustomViewAttributeDetailViewGroup extends LinearLayout {
+    public CustomViewAttributeDetailViewGroup(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
